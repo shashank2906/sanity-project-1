6c0e0737711cd7236cf12cadad2a86c7e4fc5925
shashank rai <shashank.rai00331@gmail.com>
2022-12-31 11:41:31 +0530

    started

---
+++ schemas/about.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'about',
+    type: 'document',
+      title: 'About',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      }
+    ]
+  }
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import about from './about'
+
+export const schemaTypes = [about]
